bb4c036bcdcf304999dd30eb8d8a03938cb52b80
Silas Eduardo <silasn1@live.com>
2023-09-28 06:16:26 -0300

    att

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+        
 app = Flask(__name__)
 
 @app.route('/inicio') #definição da rota de aplicação
 
 def ola():
-    return render_template('lista.html')
+    jogo1 = Jogo('bom de guerra', 'ação', 'ps5')
+    jogo2 = Jogo('mentiras de pi', 'ação', 'xbox')
+    lista = [jogo1, jogo2]
+    return render_template('lista.html', jogos = lista)
 
 app.run() #iniciar projeto
