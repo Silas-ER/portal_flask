ed1e5dd49f91241bef44a8a33cd4af525e3962d8
Silas Eduardo <silasn1@live.com>
2023-10-04 18:24:56 -0300

    att

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session, flash
 
 class User:
     def __init__(self, nome, senha, departamento):
@@ -12,6 +12,7 @@ first_user = User("silas", "12345", "TI")
 listaUsers.append(first_user)
 
 app = Flask(__name__)
+app.secret_key = 'produmar' #criptografar
 
 #pagina inicial de login
 @app.route('/login') #definição da rota de aplicação
@@ -19,17 +20,27 @@ def login():
     return render_template('login.html')
 
 #rota para login de usuario
-@app.route('/logar', methods=['GET', 'POST'])
+@app.route('/logar', methods=['GET','POST'])
 def logar():
     username = request.form['username']
     password = request.form['password']
 
     for user in listaUsers:
         if user.nome == username and user.senha == password:
+            session['usuario_logado'] = username #manter logado
             return redirect('/home')
-    
-    return render_template('login.html', error_message="Usuário não encontrado!")
+        else:
+            flash('Usuário ou senha incorretas!') #inputar mensagens
+            return redirect('/login')
 
+#rota para deslogar
+@app.route('/deslogar')
+def deslogar():
+    session['usuario_logado'] = None
+    flash('Saiu com sucesso!')
+    return redirect('/login')
+
+#rota criar usuario
 @app.route('/criar', methods=['GET', 'POST'])
 def criar():     
     return render_template('criar_user.html')
@@ -44,13 +55,10 @@ def salvarnovo():
     listaUsers.append(usuario)
     return redirect('/login')
 
-#página de relatórios
-@app.route('/relatorios', methods=['GET', 'POST'])
-def relatorios():
-    return render_template('relatorios.html')
-
 @app.route('/home', methods=['GET',])
 def home():
+    if 'usuario_logado' not in session or session['usuario_logado'] = None:
+        return redirect('/login')
     return render_template('home.html')
 
 app.run() #iniciar projeto 
